88c212e4687eeb9396366b47b2d1a98dd0954ca8
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2019-11-24 13:12:50 -0300

    Config devise to athenticate with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :videos, dependent: :destroy
 
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
