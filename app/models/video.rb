39de89ab9a393e3ea328c6a64e06fd67eb2b7f7a
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2019-11-24 15:04:09 -0300

    change informations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :user
-  delegate :email, to: :user, prefix: true
+  delegate :username, to: :user, prefix: true
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
 
   validates :link, presence: true, format: YT_LINK_FORMAT
