23d790fbdfc1c27cbb3afa3364f12cfa8769db78
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2019-11-24 14:50:10 -0300

    Change email to username in authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ApplicationRecord
   has_many :videos, dependent: :destroy
 
-  validates :email, uniqueness: true
-  validates :username, uniqueness: true
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+protected
+  def email_required?
+   false
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if username = conditions.delete(:username)
+      where(conditions.to_h).where(["lower(username) = :value ", { :value => username.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
