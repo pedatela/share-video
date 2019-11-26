7f3d8e6f6e74a3490c66cebb73dd834d1a4cbe5d
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2019-11-26 19:24:33 -0300

    Code Identetion and rspec gemfile

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,26 +3,26 @@ class VideosController < ApplicationController
 
 	def index
 			@videos = Video.order('created_at DESC')
-		end
+	end
 	
-		def new
-			@video = Video.new
+	def new
+		@video = Video.new
+	end
+
+	def create
+		@video = Video.new(video_params)
+		@video.user_id = current_user.id
+		if @video.save
+			flash[:success] = 'Video added!'
+			redirect_to root_url
+		else
+			render :new
 		end
+	end
+		
+	private
 	
-		def create
-			@video = Video.new(video_params)
-			@video.user_id = current_user.id
-			if @video.save
-				flash[:success] = 'Video added!'
-				redirect_to root_url
-			else
-				render :new
-			end
-		end
-		
-		private
-		
-		def video_params
-			params.require(:video).permit(:link)
-		end
+	def video_params
+		params.require(:video).permit(:link)
+	end
 end
