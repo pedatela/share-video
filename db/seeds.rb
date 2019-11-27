af818043c1d17ae9bab9eb8a7b6d59b768a41e67
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2019-11-26 21:30:06 -0300

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+  :email   => "admin@admin",
+  :password  => "123456",
+  :password_confirmation => "123456",
+  :username => "ADMIN"
+)
+user.save!
+
+videos = [
+  {link: "https://www.youtube.com/watch?v=VcjzHMhBtf0"},
+  {link: "https://www.youtube.com/watch?v=n_GFN3a0yj0"},
+  {link: "https://www.youtube.com/watch?v=JkK8g6FMEXE"},
+  {link: "https://www.youtube.com/watch?v=SRvCvsRp5ho"},
+  {link: "https://www.youtube.com/watch?v=Kc71KZG87X4"},
+]
+videos.each do |video|
+  video = Video.new(
+    :link  => video[:link],
+    :user_id  => 1
+  )
+  video.save!
+end
